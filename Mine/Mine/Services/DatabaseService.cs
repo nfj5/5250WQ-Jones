d2d0c6d35b9bd892eab674c30f07f5f6ce73eab9
Nicholas Jones <jonesn6@seattleu.edu>
2020-02-12 11:00:59 -0800

    Create DatabaseService (with initialize)

---
+++ Mine/Mine/Services/DatabaseService.cs
@@ -0,0 +1,54 @@
+﻿using SQLite;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Mine.Models;
+
+namespace Mine.Services
+{
+    /// <summary>
+    /// Database Services
+    /// Will write to the local data store
+    /// </summary>
+    public class DatabaseService
+    {
+        /// <summary>
+        /// Set the class to load on demand
+        /// Saves app boot time
+        /// </summary>
+        static readonly Lazy<SQLiteAsyncConnection> lazyInitializer = new Lazy<SQLiteAsyncConnection>(() =>
+        {
+            return new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        });
+
+        static SQLiteAsyncConnection Database => lazyInitializer.Value;
+        static bool initialized = false;
+
+        /// <summary>
+        /// Constructor
+        /// All the database to start up
+        /// </summary>
+        public DatabaseService()
+        {
+            InitializeAsync().SafeFireAndForget(false);
+        }
+
+        /// <summary>
+        /// Create the Table if it does not exist
+        /// </summary>
+        /// <returns></returns>
+        async Task InitializeAsync()
+        {
+            if (!initialized)
+            {
+                if (!Database.TableMappings.Any(m => m.MappedType.Name == typeof(ItemModel).Name))
+                {
+                    await Database.CreateTablesAsync(CreateFlags.None, typeof(ItemModel)).ConfigureAwait(false);
+                    initialized = true;
+                }
+            }
+        }
+
+    }
+}
