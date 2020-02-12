cb7a3eb923bc0702453f59182702fc77cdd6f39f
Nicholas Jones <jonesn6@seattleu.edu>
2020-02-12 11:53:23 -0800

    AboutPage data source switcher XAML and event handlers

--- Mine/Mine/Views/AboutPage.xaml.cs
+++ Mine/Mine/Views/AboutPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using Xamarin.Forms;
 
 namespace Mine.Views
@@ -19,6 +20,38 @@ namespace Mine.Views
             InitializeComponent();
 
             CurrentDateTime.Text = System.DateTime.Now.ToString("MM/dd/yy hh:mm:ss");
+        }
+
+        /// <summary>
+        /// Event handler for switching the data source
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void SQLDataSourceSwitch_OnToggled(object sender, ToggledEventArgs e)
+        {
+            if (DataSourceValue.IsToggled)
+            {
+                MessagingCenter.Send(this, "SetDataSource", 1);
+            }
+            else
+            {
+                MessagingCenter.Send(this, "SetDataSource", 0);
+            }
+        }
+
+        /// <summary>
+        /// Event handler for wiping the datastore
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void WipeDataList_Clicked(object sender, EventArgs e)
+        {
+            bool answer = await DisplayAlert("Delete Data", "Are you sure that you want to delete all data?", "Yes", "No");
+
+            if (answer)
+            {
+                MessagingCenter.Send(this, "WipeDataList", true);
+            }
         }
     }
 }
